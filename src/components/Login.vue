9023b7bda2d2662bece09b226ab7c824476b0540
ofojichigozie <ofojichigozie@gmail.com>
2022-11-19 19:32:28 +0100

    UI modifications on login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="main">
-    <h2 class="heading text-center text-dark" style="padding: 1em 2em;">
-        Agricultural Commodity Price Prediction System
+    <h2 class="heading text-center text-dark mt-4" style="padding: 1em 2em; color: #211c46;">
+        Commodity Price Forecasting System
     </h2>
    
     <div class="d-flex justify-content-center">
         <div class="form-div">
-            <h4 class="text-center">Login</h4>
+            <h4 class="text-center" style="color: #211c46">
+                Login
+            </h4>
             <hr>
-            <h5 class="text-center" style="color: #FF1212;">{{ errorMessage }}</h5>
+            <h6 class="text-center" style="color: #FF1212;">
+                {{ errorMessage }}
+            </h6>
             <form @submit.prevent="login">
                 <label for="username">Email</label><br>
                 <input type="email" name="email" v-model="email" id="email"><br>
                 <label for="password">Password</label><br>
                 <input type="password" name="password" v-model="password" id="password"><br>
-                <input v-show="!isProcessing" type="submit" class="btn btn-dark" value="Submit"><br>
+                <input v-show="!isProcessing" type="submit" class="btn btn-dark" value="Continue"><br>
                 <div v-show="isProcessing" class="text-center">
                     <img v-show="true" src="/images/loading.gif" alt="Loading">
                 </div>
@@ -67,7 +71,7 @@
 <style scoped>
     .form-div{
         margin-top: 3%;
-        background-color: #FFFFFF;
+        background-color: #f2f2ff;
         color: #000000 !important;
         border: 1px solid #CCCCCC;
         padding: 1.5em;
@@ -79,7 +83,15 @@
         width: 100%;
         height: 40px;
         padding: 10px;
+        border-radius: 5px;
+        outline: 1px #a093ff solid;
+        border: none;
         margin: 10px 0px;
+    }
+
+    input[type="submit"]{
+        background-color: #211c46;
+        color: #ffffff;
     }
 
     @media screen and (min-width: 760px){
